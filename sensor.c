fce079bea6ddc2bffd200499c5c95773610b7d01
Jordan-Sun <sk.jordan@foxmail.com>
2022-03-08 17:41:55 -0600

    Added LICENSE header to sensor.c

--- sensor.c
+++ sensor.c
@@ -1,9 +1,23 @@
-/*
+/* 
  * sensor.c
  * 
- * Process the data from an indoor and an
- * outdoor tsl2561 ambient light sensor.
+ * Reads the data from an indoor and an outdoor tsl2561 ambient light
+ *  sensor and process it.
  * 
+ * Copyright (C) 2022 Jordan Sun
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <linux/i2c-dev.h>
