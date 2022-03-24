8ef0cef58479da750c41da428f2e15e204f61b6c
Jordan <sk.jordan@foxmail.com>
2022-03-24 13:42:03 -0500

    Motor packet

---
+++ src/motor_packet.h
@@ -0,0 +1,35 @@
+/*
+ * motor_packet.h
+ *
+ * The data packet sent from the control thread to the motor thread.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MOTOR_PACKET_H
+#define MOTOR_PACKET_H
+
+#include <time.h>
+
+typedef struct motor_packet
+{
+    /* Metadata */
+    struct timespec timestamp;
+    unsigned int sequence;
+    /* Data */
+    int direction;
+    int turns;
+} motor_packet_t;
+
+#endif /* MOTOR_PACKET_H */
