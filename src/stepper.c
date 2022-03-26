7f3b2101592321d3a61fc4d9af3913671f979a3a
wzk <w.zhikuan@wustl.edu>
2022-03-26 17:29:16 -0500

    motor code

---
+++ src/stepper.c
@@ -0,0 +1,363 @@
+#include "stepper.h"
+
+void usage(char* progname) {
+    printf("usage: %s <ENABLE_NUM> <DIR_NUM> <PUL_NUM> <DIRECTION> <PORT_NUM>\n", progname);
+}
+
+int msleep(long tms)
+{
+    struct timespec ts;
+    int ret;
+
+    if (tms < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = tms / 1000;
+    ts.tv_nsec = (tms % 1000) * 1000000;
+
+    do {
+        ret = nanosleep(&ts, &ts);
+    } while (ret && errno == EINTR);
+
+    return ret;
+}
+
+void rotate(int rotations, int pul_fd) {
+    if (rotations > 1000 || rotations <= 0) {
+        perror("rotations should be in (0, 100]\n");
+        return;
+    }
+    for (int i = 1; i <= rotations; i++) {
+        write(pul_fd, HIGH, 1);
+        msleep(INTERVAL);
+        write(pul_fd, LOW, 1);
+        msleep(INTERVAL);
+        if (i % 5 == 0) {
+            printf("already rotates %d/%d\n", i, rotations);
+        }
+    }
+    printf("rotation finished.\n");
+}
+
+int islegal(char* target) {
+    if (strlen(target) > 10) {
+        perror("input should be at most 10 characters.\n");
+        return 0;
+    }
+    for (int i = 0; i < strlen(target); ++i) {
+        if (target[i] < '0' || target[i] > '9') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int set_direction(int direction, int dir_fd) {
+    if (direction != 0 && direction != 1) {
+        perror("direction should be 0 or 1.\n");
+        return 0;
+    }
+    char* dir = direction == 1 ? "1" : "0";
+    int ret = write(dir_fd, dir, 1);
+    if (ret < 0) {
+        printf("Error: %s\n", strerror(errno));
+        return 0;
+    }
+    return 1;
+}
+
+int export(char** argv) {
+    int fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open /sys/class/gpio/export");
+        printf("error: %s\n", strerror(errno));
+        return 0;
+    }
+
+    for (int i = ENABLE_NUM; i <= PUL_NUM; ++i) {
+        if (write(fd, argv[i], 2) <= 0) {
+            printf("Unable to write %s to /sys/class/gpio/export\n", argv[i]);
+            printf("error: %s\n", strerror(errno));
+            return 0;
+        }
+    }
+    close(fd);
+    return 1;
+}
+
+void unexport(char **argv) {
+    int fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open /sys/class/gpio/unexport");
+        printf("error: %s\n", strerror(errno));
+    }
+
+    for (int i = ENABLE_NUM; i <= PUL_NUM; ++i) {
+        if (write(fd, argv[i], 2) < 0) {
+            printf("Unable to write %s to /sys/class/gpio/unexport\n", argv[i]);
+            printf("error: %s\n", strerror(errno));
+        }
+    }
+    close(fd);
+}
+
+// #IFDEF __DEBUG_STEPPER
+int main(int argc, char **argv) {
+    int rotations = 0, fd, ret, epoll_fd, pulse_fd, direction_fd;
+    int connection_socket, ready, need_quit = 0, dir;
+    unsigned int port_number;
+    char buf[BUFSZ];
+    // used socket
+    struct sockaddr_in local_socket, in_socket;
+    socklen_t addr_len;
+    char server_host_name[NI_MAXHOST];
+    struct epoll_event ev, evlist[EVENT_SIZE];
+    motor_packet_t in_packet;
+
+    // usage
+    if (argc != ARGS_NUMBER) {
+        usage(argv[PROGRAM_NAME]);
+        exit(EXIT_FAILURE);
+    }
+
+    // in case those gpio are opened by others.
+    unexport(argv);
+
+    if (!export(argv)) {
+        perror("export failed!");
+        exit(EXIT_FAILURE); 
+    }
+
+    printf("Start dir initialization.\n");
+
+    //initialize direction
+    for (int i = ENABLE_NUM; i <= PUL_NUM; ++i) {
+        ret = sprintf(buf, DIR_FMT, argv[i]);
+        if (ret < 0) {
+            printf("Error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        printf("initializing %s\n", buf);
+        fd = open(buf, O_WRONLY);
+        if (fd == -1) {
+            printf("error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        ret = write(fd, OUT, 3);
+        if (ret < 0) {
+            printf("Error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        close(fd);
+    }
+
+    printf("End dir initialization.\n");
+    printf("Start value initialization.\n");
+
+    //initialize value
+    for (int i = ENABLE_NUM; i < PUL_NUM; ++i) {
+        ret = sprintf(buf, VALUE_FMT, argv[i]);
+        if (ret < 0) {
+            printf("Error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        printf("initializing %s\n", buf);
+        fd = open(buf, O_WRONLY);
+        if (fd == -1) {
+            printf("error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        // TODO error handle
+        if (i == DIR_NUM) {
+            direction_fd = fd;
+            if (strcmp(argv[DIRECTION], "+") == 0)
+                ret = write(fd, HIGH, 1);
+            else
+                ret = write(fd, LOW, 1);
+        } else {
+            ret = write(fd, HIGH, 1);
+            close(fd);
+        }
+        if (ret < 0) {
+            printf("Error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    printf("End value initialization.\n");
+    printf("start pulse initialization.\n");
+
+    ret = sprintf(buf, VALUE_FMT, argv[PUL_NUM]);
+    if (ret < 0) {
+        printf("Error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    pulse_fd = open(buf, O_WRONLY);
+    if (pulse_fd < 0) {
+        printf("Error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    ret = write(pulse_fd, LOW, 1);
+    if (ret < 0) {
+        printf("Error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    printf("End pulse initialization.\n");
+
+    // ----------------------------------------
+    // convert port number to unsigned int.
+    port_number = atoi(argv[PORT_NUMBER]);
+    
+    // create a socket for local connection.
+    connection_socket = socket(AF_INET, SOCK_STREAM, SOCKET_PROTOCAL);
+    // on error, -1 is returned.
+    if (connection_socket < 0) {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    printf("Created connection socket %u.\n", connection_socket);
+
+    // connected, get name info
+    ret = gethostname(server_host_name, sizeof(server_host_name));
+    // on error, -1 is returned.
+    if (ret < 0) {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    printf("Hosting on %s on port %u.\n", server_host_name, port_number);
+
+    // create the communications channel.
+    memset(&local_socket, 0, sizeof(struct sockaddr_in));
+    local_socket.sin_family = AF_INET;
+    local_socket.sin_port = htons(port_number);
+    local_socket.sin_addr.s_addr = INADDR_ANY;
+    ret = bind(connection_socket, (const struct sockaddr *) &local_socket, sizeof(struct sockaddr_in));
+    // on error, -1 is returned.
+    if (ret < 0) {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    // start listening on connection socket.
+    ret = listen(connection_socket, SOCKET_BACKLOG_SIZE);
+    // on error, -1 is returned.
+    if (ret < 0) {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+	printf("Start listening on connection socket %u.\n", connection_socket);
+
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0)  {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ev.data.fd = connection_socket;
+    ev.events = EPOLLIN;
+    ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connection_socket, &ev);
+    if (ret < 0)  {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    ev.data.fd = STDIN_FILENO;
+    ev.events = EPOLLIN;
+    ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &ev);
+    if (ret < 0)  {
+        printf("error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Waiting for command.\n");
+
+
+    // listen command
+    // TODO socket module
+    // while loop for accept socket connections.
+    do {
+        //epoll will block until a connection is ready for I/O
+        if ((ready = epoll_wait(epoll_fd, evlist, EVENT_SIZE, -1)) > 0) {
+            if (ready < 0 && errno != EINTR) {
+                perror("epoll_wait wrong.");
+            }
+            for (int i = 0; i < ready; i++) {
+                // input is ready
+                if (evlist[i].events & EPOLLIN) {
+                    // new connection.
+                    if (evlist[i].data.fd == connection_socket) {    
+                        printf("Detected new connection\n");
+                        if ((fd = accept(connection_socket, (struct sockaddr *)&in_socket, &addr_len)) < 0) {
+                            printf("error: %s\n", strerror(errno));
+                            continue;
+                        }
+                        printf("Incomming connection: %s port:%u\n", inet_ntoa(in_socket.sin_addr), in_socket.sin_port);
+                        // register the new socket into epoll
+                        ev.events = EPOLLIN | EPOLLRDHUP;
+                        ev.data.fd = fd;
+                        ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+                        if (ret < 0) {
+                            printf("error: %s\n", strerror(errno));
+                        }
+
+                    // data from stdin
+                    } else if (evlist[i].data.fd == STDIN_FILENO) {
+                        ret = read(STDIN_FILENO, buf, BUFSZ);
+                        if (ret < 0) {
+                            printf("Error: %s\n", strerror(errno));
+                            continue;
+                        }
+                        buf[ret-1] = '\0';
+                        printf("input: %s\n", buf);
+                        if (strcmp(buf, "q") == 0 || strcmp(buf, "quit") == 0) {
+                            need_quit = 1;
+                            break;                       
+                        } else if (islegal(buf)) {
+                            rotations = atoi(buf);
+                            rotate(rotations, pulse_fd);
+                        } 
+
+                    // data from client.
+                    } else {
+                        fd = evlist[i].data.fd;
+                        printf("Get command from client\n");
+                        ret = read(fd, &in_packet , sizeof(motor_packet_t));
+                        if (ret < 0) {
+                            printf("Error: %s\n");
+                            continue;
+                        }
+                        dir = in_packet.direction;
+                        rotations = in_packet.turns;
+                        printf("Receiving command to rotate motor %d pulses in %d direction\n", rotations, dir);
+                        ret = set_direction(dir, direction_fd);
+                        if (!ret) continue;
+                        rotate(rotations, pulse_fd);
+                    }
+                // peer disconnected.
+                } else if (evlist[i].events & EPOLLRDHUP) {
+                    fd = evlist[i].data.fd;
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+                    close(fd);
+                }
+            }
+        }
+    } while (!need_quit);
+
+    close(connection_socket);
+    close(pulse_fd);
+    close(direction_fd);
+    unexport(argv);
+
+    return 0;
+
+}
+// #ENDIF
+
+// #IFNDEF __DEBUG_STEPPER
+// int main() {
+
+// }
+// #ENDIF
---
+++ src/stepper.h
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <time.h>
+#include "motor_packet.h"
+// for unix
+#include <sys/un.h>
+// for remote
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+//for epoll
+#include <sys/epoll.h>
+
+
+#define HIGH "1"
+#define LOW "0"
+#define OUT "out"
+#define INTERVAL 100
+#define BUFSZ 128 
+#define VALUE_FMT "/sys/class/gpio/gpio%s/value"
+#define DIR_FMT "/sys/class/gpio/gpio%s/direction"
+#define EVENT_SIZE 4
+#define SOCKET_PROTOCAL 0
+#define SOCKET_BACKLOG_SIZE 255
+
+// #define __DEBUG_STEPPER 1
+
+enum ARGS{
+    PROGRAM_NAME,
+    ENABLE_NUM,
+    DIR_NUM,
+    PUL_NUM,
+    DIRECTION,
+    PORT_NUMBER,
+    ARGS_NUMBER
+};
+
+// /** initialize the config of stepper motor.
+// * input should be the bcm number of corresponding gpio pins.
+// * output will be the pulse fd if export successfully. 
+// **/
+// int motor_export(int enable, int direction, int pulse);
+
+// /** free the gpio resource
+//  * input should be the bcm number of corresponding gpio pins
+//  * return 1 if unexport successfully, otherwise return 0.
+//  **/
+// int motor_unexport(int enable, int direction, int pulse);
+
+// /** control how many pulses will be send to rotation in a given internal.
+//  * input should contain the corresponding pulse fd.
+//  * return -1 for error.
+//  **/
+// int motor_rotate(int rotations, int pul_fd, int interval_ms);
+
+// /** control the direction of stepper.
+//  * 1 for clock wise, -1 for counter clock wise
+//  * return -1 for error.
+//  **/
+// int motor_direction(int cw, int direction_pin);
